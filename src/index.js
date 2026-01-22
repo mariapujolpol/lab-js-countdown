55e4a08616b1b51d77b8caa956961cd65ea064f9
Maria Pol <maria.pujolpol@gmail.com>
2026-01-22 17:57:43 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
+let btnCounter = document.querySelector("#start-btn");
+btnCounter.addEventListener("click", startCountdown);
+
+
+
+
+
+
+
 // Your code goes here ...
 
 
@@ -17,6 +26,28 @@ function startCountdown() {
 
 
   // Your code goes here ...
+
+  const timeDiv = document.querySelector("#time");
+  const startBtn = document.querySelector("#start-btn");
+
+  // Disable the start button to prevent multiple clicks
+  startBtn.disabled = true;
+  timer = setInterval(function () {
+
+  timeDiv.innerText = remainingTime;
+  remainingTime--;
+
+  if (remainingTime < 0) {
+    clearInterval(timer);
+
+    remainingTime = DURATION; // Reset remaining time
+    timeDiv.innerText = remainingTime;
+    showToast("Lift off! 🚀"); // Show toast message
+    startBtn.disabled = false; // Re-enable the start button
+    let toastTimeOut = null;
+}
+}, 1000);
+
 }
 
 
@@ -27,6 +58,15 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
+  const toast = document.querySelector("#toast");
+  const toastMessage = document.querySelector("#toast-message");
+
+  toastMessage.innerText = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
 
 
 
